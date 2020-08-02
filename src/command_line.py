2c3ad5e59a4181629c4e201259503b2c36c97380
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2020-08-02 16:19:13 +0100

    started working on adding a config file to store default arguments

--- src/command_line.py
+++ src/command_line.py
@@ -1,6 +1,7 @@
 from github import Github
 import os
 import argparse
+import configparser
 
 token = os.environ.get("automate_github_project_start_token")
 
@@ -14,20 +15,15 @@ def create_github_repo():
     # repo = user.create_repo("test")
 
 
-def generate_repo_name_based_upon_parent_directory():
-    # parent_directory_path = os.path.dirname(os.getcwd())
+def generate_repo_name_based_upon_cwd():
     parent_directory_folder_name = os.path.basename(os.getcwd())
     parent_directory_folder_name = parent_directory_folder_name.lower().replace(" ", "_")
     return parent_directory_folder_name
 
-def get_parent_directory():
-    pass
-
-
-def parse_arguments():
-    pass
+def parse_and_return_args():
     parser = argparse.ArgumentParser(description="A script used to automatically setup a remote github repository and then setup your local repository with all the things that you require, such as a virtual env, src directory, .gitignore file, etc")
-    parser.add_argument("--create_remote_repo", metavar="name", nargs="?", const=generate_repo_name_based_upon_parent_directory(), help="create a remote repository on github with your given name, if no name is given, it will default to the name of the cwd")
+    
+    parser.add_argument("--create_remote_repo", metavar="name", nargs="?", const=generate_repo_name_based_upon_cwd(), help="create a remote repository on github with your given name, if no name is given, it will default to the name of the cwd")
     parser.add_argument("-d", "--local_directory", metavar="path", default=os.getcwd(), help="create local repository in chosen path, defaults to cwd if flag is not specified")
     parser.add_argument("-v", "--venv", action="store_true", help="create a venv")
     parser.add_argument("--docs", action="store_true", help="create a docs directory")
@@ -39,13 +35,18 @@ def parse_arguments():
     parser.add_argument("--gitignore", help="choose a gitignore template")
     parser.add_argument("-c", "--config", help="enter a config.ini file")
 
-
     args = parser.parse_args()
-    # print(args.local_directory)
+    return args
 
 
+def config_parser(args):
+    config_path = "../defaults.ini"
+    config = configparser.ConfigParser()
+    config.read_file("../defaults.ini")
+
+    # args.create_remote_repo = 
 
 
 if __name__ == "__main__":
-    pass
-    parse_arguments()
+    args = parse_and_return_args()
+    config_parser(args)
