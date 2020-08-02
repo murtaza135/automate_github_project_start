adc01dae7d196b5cfa0a7d67cf644459a3894811
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2020-08-02 18:52:22 +0100

    abstracted github code into separate class, created methods to get gitignore template files

--- command_line.py
+++ command_line.py
@@ -1,21 +1,13 @@
 from github import Github
+from my_github import MyGithub
 import os
 import argparse
 import configparser
 
-token = os.environ.get("automate_github_project_start_token")
-
-def create_github_repo():
-    pass
-    # gh = Github(token)
-    # rate_limit = gh.get_rate_limit()
-    # print(rate_limit)
-
-    # user = gh.get_user()
-    # repo = user.create_repo("test")
 
 
-def generate_repo_name_based_upon_cwd():
+
+def generate_repository_name_based_upon_cwd():
     parent_directory_folder_name = os.path.basename(os.getcwd())
     parent_directory_folder_name = parent_directory_folder_name.lower().replace(" ", "_")
     return parent_directory_folder_name
@@ -23,9 +15,9 @@ def generate_repo_name_based_upon_cwd():
 def get_args_from_terminal():
     parser = argparse.ArgumentParser(description="A script used to automatically setup a remote github repository and then setup your local repository with all the things that you require, such as a virtual env, src directory, .gitignore file, etc")
     
-    parser.add_argument("--no_remote_repo", action="store_true", help="do not create a remote github repository, only create a local repository")
-    parser.add_argument("-n", "--repo_name", metavar="name", type=str, default=generate_repo_name_based_upon_cwd(), help="choose a name for your remote github repository, defaults to name of cwd, note that this requires the --no_remote_repo flag not to be used")
-    # parser.add_argument("--create_remote_repo", metavar="name", nargs="?", const=generate_repo_name_based_upon_cwd(), help="create a remote repository on github with your given name, if no name is given, it will default to the name of the cwd")
+    parser.add_argument("--no_remote_repository", action="store_true", help="do not create a remote github repository, only create a local repository")
+    parser.add_argument("-n", "--repository_name", metavar="name", type=str, default=generate_repository_name_based_upon_cwd(), help="choose a name for your remote github repository, defaults to name of cwd, note that this requires the --no_remote_repository flag not to be used")
+    # parser.add_argument("--create_remote_repository", metavar="name", nargs="?", const=generate_repository_name_based_upon_cwd(), help="create a remote repository on github with your given name, if no name is given, it will default to the name of the cwd")
     parser.add_argument("-d", "--local_directory", metavar="path", type=str, default=os.getcwd(), help="create local repository in chosen path, defaults to cwd if flag is not specified")
     parser.add_argument("-v", "--venv", action="store_true", help="create a venv")
     parser.add_argument("--docs", action="store_true", help="create a docs directory")
@@ -43,11 +35,10 @@ def get_args_from_terminal():
 
 def get_args_from_config_file_and_overwrite_old_args(args):
     filename = args.config
-
     config = configparser.ConfigParser()
     config.read_file(filename)
 
-    args.no_remote_repo = bool(config['ARGUMENTS'].get("no_remote_repo", args.no_remote_repo))
+    args.no_remote_repository = bool(config['ARGUMENTS'].get("no_remote_repository", args.no_remote_repository))
     args.venv = bool(config['ARGUMENTS'].get("venv", args.venv))
     args.docs = bool(config['ARGUMENTS'].get("docs", args.docs))
     args.logs = bool(config['ARGUMENTS'].get("logs", args.logs))
@@ -55,7 +46,7 @@ def get_args_from_config_file_and_overwrite_old_args(args):
     args.src = bool(config['ARGUMENTS'].get("src", args.src))
     args.tests = bool(config['ARGUMENTS'].get("tests", args.tests))
     args.gitignore = str(config['ARGUMENTS'].get("gitignore", args.gitignore))
-    args.repo_name = str(config['ARGUMENTS'].get("repo_name", args.repo_name))
+    args.repository_name = str(config['ARGUMENTS'].get("repository_name", args.repository_name))
     args.local_directory = str(config['ARGUMENTS'].get("local_directory", args.local_directory))
 
     return args
@@ -67,14 +58,61 @@ if __name__ == "__main__":
     if args.config:
         args = get_args_from_config_file_and_overwrite_old_args(args)
 
-    print(args.no_remote_repo)
-    print(args.venv)
-    print(args.docs)
-    print(args.logs)
-    print(args.notes)
-    print(args.src)
-    print(args.tests)
-    print(args.gitignore)
-    print(args.repo_name)
-    print(args.local_directory)
-    print()
+    if not(args.no_remote_repository):
+        gh = MyGithub()
+        gh.create_github_repository(args.repository_name)
+
+    if args.venv:
+        pass
+        # create venv
+
+    if args.docs:
+        pass
+        # create doc dir
+
+    if args.logs:
+        pass
+        # create logs dir
+
+    if args.notes:
+        pass
+        # create notes dir
+
+    if args.src:
+        pass
+        # creates src dir
+
+    if args.tests:
+        pass
+        # create tests dir
+
+    if args.requirements:
+        pass
+        # create requirements.txt
+
+    if args.gitignore:
+        get_gitignore_file()
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # print(args.no_remote_repository)
+    # print(args.venv)
+    # print(args.docs)
+    # print(args.logs)
+    # print(args.notes)
+    # print(args.src)
+    # print(args.tests)
+    # print(args.gitignore)
+    # print(args.repository_name)
+    # print(args.local_directory)
+    # print()
---
+++ my_github.py
@@ -0,0 +1,22 @@
+from github import Github
+
+class MyGithub:
+
+    def __init__(self):
+        pass
+
+    def create_github_repository(self, repository_name):
+        token = os.environ.get("automate_github_project_start_token")
+
+        self.gh = Github(token)
+
+        self.user = gh.get_user()
+        self.repository = user.create_repository(repository_name)
+
+    @staticmethod
+    def get_all_gitignore_templates():
+        return Github.get_gitignore_templates()
+
+    @staticmethod
+    def get_specific_gitignore_template(template_name):
+        return Github.get_gitignore_template(template_name)
